b5180d0e9712f49dd6cf5bedf985dd30c57a0d16
Rodolfo Moreira <rodolfo.notalab@gmail.com>
2026-03-24 20:22:44 -0300

    Criação da tabela silver vendas

---
+++ macros/typing.sql
@@ -0,0 +1,15 @@
+{% macro cast_int(col) -%}
+    CAST(SPLIT({{ col }}, '\\.')[0] AS BIGINT)
+{%- endmacro %}
+
+{% macro cast_decimal(col) -%}
+    CAST(REPLACE({{ col }}, ',', '.') AS FLOAT)
+{%- endmacro %}
+
+{% macro cast_timestamp(col) -%}
+    TO_TIMESTAMP({{ col }})
+{%- endmacro %}
+
+{% macro cast_timestamp(col) -%}
+    TO_DATE({{ col }})
+{%- endmacro %}
